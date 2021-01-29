c7e2b1fdc3b86751975dcc1fe7696e086c6e65b5
matthewslima22 <matthewssilva96@gmail.com>
2021-01-29 14:40:11 -0300

    Project created

---
+++ conpay/src/main/java/com/desafioconpay/conpay/ConpayApplication.java
@@ -0,0 +1,13 @@
+package com.desafioconpay.conpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConpayApplication.class, args);
+	}
+
+}
---
+++ conpay/src/test/java/com/desafioconpay/conpay/ConpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioconpay.conpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
